b6fcf907a5d55cba5d2d79515c17190d8bae14a6
WinPC <17307110223@fudan.edu.cn>
2022-06-06 22:30:33 +0800

    log fixed by Tianyi Zhang mysql modified for delivery-service

--- ts-delivery-service/src/main/java/delivery/mq/RabbitReceive.java
+++ ts-delivery-service/src/main/java/delivery/mq/RabbitReceive.java
@@ -43,7 +43,7 @@ public class RabbitReceive {
         logger.info("Receive delivery object:" + delivery);
 
         if (delivery.getId() == null) {
-            delivery.setId(UUID.randomUUID());
+            delivery.setId(UUID.randomUUID().toString());
         }
 
         try {
--- ts-delivery-service/src/main/java/delivery/repository/DeliveryRepository.java
+++ ts-delivery-service/src/main/java/delivery/repository/DeliveryRepository.java
@@ -1,19 +1,17 @@
 package delivery.repository;
 
 import delivery.entity.Delivery;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface DeliveryRepository extends MongoRepository<Delivery, String> {
+public interface DeliveryRepository extends CrudRepository<Delivery, String> {
 
     Delivery findById(UUID id);
 
-    @Query("{ 'orderId' : ?0 }")
     Delivery findByOrderId(UUID orderId);
 
     @Override
